9159ccbe9251c3b5884ed1b4207f549af9cbf7ee
Andres <andrescasa.dev@gmail.com>
2022-11-08 12:30:47 -0500

    meme generator: prev and next

--- memeGenerator/src/components/Main.jsx
+++ memeGenerator/src/components/Main.jsx
@@ -1,11 +1,36 @@
-import {useState} from React;
+import {useState, useEffect} from "react";
 import ToolBar from "./Toolbar"
 import MemeSlider from "./MemeSlider"
 export default function Main(){
+  const [memeList, setMemeList] = useState([])
+  const [memeIndex, setMemeIndex] = useState(0);
+  const DEFAULT_IMG = "https://securecdn.pymnts.com/wp-content/uploads/2016/06/Slow-load-times-eCommerce.png"
+  function nextMeme(){
+    setMemeIndex((prevIndex)=>{
+      return prevIndex < memeList.length - 1 ? prevIndex + 1 : prevIndex
+    })
+  }
+
+  function prevMeme(){
+    setMemeIndex((prevIndex)=>{
+      return prevIndex > 0 ? prevIndex - 1 : prevIndex
+    })
+  }
+
+  function getMemeSrc(){
+    return memeList[memeIndex] ? memeList[memeIndex].url : false;
+  }
+
+  useEffect(()=>{
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response)=> response.json())
+      .then((memeData)=> setMemeList(memeData.data.memes))
+  },[])
+
   return (
     <div className="mt-6 flex flex-col col-start-2 col-end-3 items-center relative">
         <ToolBar/>
-        <MemeSlider src=""/>
+        <MemeSlider src={getMemeSrc() || DEFAULT_IMG} handleNext={nextMeme} handlePrev={prevMeme}/>
     </div>
   )
 }
--- memeGenerator/src/components/MemeSlider.jsx
+++ memeGenerator/src/components/MemeSlider.jsx
@@ -1,13 +1,14 @@
 import arrow from "../assets/arrow.svg"
 
-export default function MemeSlider({src}){
+export default function MemeSlider({src, handleNext, handlePrev}){
+  //https://i.imgflip.com/1g8my4.jpg
   return (
-    <div className="flex gap-8 justify-center md:mt-4 relative">
-      <button className="hidden self-start mt-48 md:block"><img src={arrow} alt="previous" /></button>
+    <div className="md:w-[60&] w-[80%] flex gap-8 justify-center md:mt-4 relative">
+      <button onClick={handlePrev} className="hidden self-start mt-48 md:block"><img src={arrow} alt="previous" /></button>
       <input className="text-stroke absolute top-9 font-meme text-5xl font-bold text-white bg-transparent outline-none placeholder-white text-center" type="text" placeholder="Write something" />
-      <img className="md:w-full w-3/4" src="https://i.imgflip.com/1g8my4.jpg" alt="meme" />
+      <img className="md:w-1/2 w-3/4" src={src} alt="meme" />
       <input className="text-stroke absolute bottom-9 font-meme text-5xl font-bold text-white bg-transparent outline-none placeholder-white text-center" type="text" placeholder="Write something" />
-      <button className="hidden self-start mt-48 md:block"><img className="rotate-180" src={arrow} alt="next" /></button>
+      <button onClick={handleNext} className="hidden self-start mt-48 md:block"><img className="rotate-180" src={arrow} alt="next" /></button>
     </div>
   )
 }
